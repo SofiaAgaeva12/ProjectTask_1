37a11e335ae178277308440b20c8f98b0e800a05
sofia agaeva <sofiaagaeva4574@gmail.com>
2022-11-06 22:19:41 +0700

    end

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,11 +1,11 @@
 let button_activate_menu = document.getElementById('button')
-let content = document.getElementsByClassName('header_block')[0];
+let content = document.getElementsByClassName('header__block')[0];
 function modifyClass() {
-    document.getElementsByClassName('menu_items')[0].style.display = "flex";
+    document.getElementsByClassName('menu__items')[0].style.display = "flex";
     document.getElementById('number-info').style.display = "none";
 }
 function returnClass() {
-    document.getElementsByClassName('menu_items')[0].style.display = "none";
+    document.getElementsByClassName('menu__items')[0].style.display = "none";
     document.getElementById('number-info').style.display = "block";
 }
 button_activate_menu.addEventListener("click", modifyClass);
