a71919deaf670de3aebba5f904b423a7f5618763
SofiaAgaeva12 <sofiaagaeva45744@gmail.com>
2022-11-07 12:12:47 +0700

    menu & adaptive

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,12 +1,9 @@
 let button_activate_menu = document.getElementById('button')
-let content = document.getElementsByClassName('header__block')[0];
-function modifyClass() {
-    document.getElementsByClassName('menu__items')[0].style.display = "flex";
-    document.getElementById('number-info').style.display = "none";
+let menu = document.getElementsByClassName('menu__items')[0]
+function addClass() {
+    menu.classList.toggle("menu__items-flex")
+  
 }
-function returnClass() {
-    document.getElementsByClassName('menu__items')[0].style.display = "none";
-    document.getElementById('number-info').style.display = "block";
-}
-button_activate_menu.addEventListener("click", modifyClass);
-content.addEventListener("click", returnClass);
+
+button_activate_menu.addEventListener("click", addClass);
+
