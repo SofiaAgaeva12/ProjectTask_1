9ce583e47f455bfa95ac44fb71a03d5a07a460f2
SofiaAgaeva12 <sofiaagaeva45744@gmail.com>
2022-11-10 12:25:30 +0700

    function on js (output data from inputs)

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -1,9 +1,17 @@
 let button_activate_menu = document.getElementById('button')
 let menu = document.getElementsByClassName('menu__items')[0]
+let header = document.getElementsByTagName('header')
+let content = document.getElementById('headerNone')
+let numberInfo = document.getElementById('number-info')
 function addClass() {
     menu.classList.toggle("menu__items-flex")
-  
+    header[0].style.background = 'white'
+    content.style.display = 'none'
+    numberInfo.style.display = 'none'
+    if (!menu.classList.contains('menu__items-flex')) {
+        content.style.display = 'block'
+        numberInfo.style.display = 'block'
+    }
 }
 
 button_activate_menu.addEventListener("click", addClass);
-
